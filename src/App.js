7150a80455ee1a36a1819338fdd57778d2ec0dc5
Deepayan <deepayanmukherjee1312@gmail.com>
2021-07-11 08:33:40 +0530

    Added login page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react'
+import Login from "./pages/Login/Components/Login"
+import SignUp from "./pages/Login/Components/SignUp"
 import AdultQuiz from "./pages/quiz/adultQuiz"
 import KidsQuiz from "./pages/quiz/kidsQuiz"
 import FunZone from "./pages/funZone/funZone"
+import {BrowserRouter as Router,Route, Switch} from "react-router-dom"
 
 export default function App() {
 	return (
 		<div>
-			{/* <AdultQuiz /> */}
-			{/* <KidsQuiz /> */}
-			<FunZone />
+			<Router>
+				<Route exact path="/">
+					<SignUp />
+				</Route>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route path="/adultquiz">
+					<AdultQuiz />
+				</Route>
+				<Route path="/kidsquiz">
+					<KidsQuiz />
+				</Route>
+			</Router>
+			
 		</div>
 	)
 }
